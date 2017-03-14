9135fbdf99f15de75e3d3bf867bd85aa04c1cfbf
GBK4 <grat.king@gmail.com>
2017-03-13 20:43:42 -0700

    Add files via upload

--- Lab4.cpp
+++ Lab4.cpp
@@ -9,14 +9,41 @@ Modified:		3/13/2017
 
 -------------------------------------------------------------------------------------------------*/
 
-
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include "storemanager.h"
+
 
 using namespace std;
 
 int main() {
 
+	ifstream infileCust("data4customers.txt");
+	if (!infileCust) {
+		cout << "Customer file could not be opened." << endl;
+		return 1;
+	}
+
+	ifstream infileMov("data4movies.txt");
+	if (!infileMov) {
+		cout << "Inventory file could not be opened." << endl;
+		return 1;
+	}
+	
+	ifstream infileCom("data4commands.txt");
+	if (!infileCom) {
+		cout << "Transaction file could not be opened." << endl;
+		return 1;
+	}
+
+	StoreManager Store;
+
+	Store.setCustomers(infileCust);
+	Store.setInventory(infileMov);
+	Store.setTransactions(infileCom);
+
+	Store.processTransactions();
 	
 	return 0;
 }
