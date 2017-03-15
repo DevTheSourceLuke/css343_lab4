36f8f44194dd23e50efd3d944877aa28065ae25e
Luke <luke429@uw.edu>
2017-03-14 17:37:03 -0700

    Add files via upload

---
+++ hashtable.cpp
@@ -0,0 +1,75 @@
+/*-------------------------------------------------------------------------------------------------
+
+Authors:		Bushey, Luke
+King, Garret
+
+Created:		2/21/2017
+Modified:		3/1/2017
+
+HashTable class that has the functionality to populate a HashTable, retrieve items,
+and insert items.
+
+NOTES:	All responsibility for handling a filestream to populate an Object's data members
+is the responsibility of the Object.
+
+-------------------------------------------------------------------------------------------------*/
+
+#pragma once
+#include "hashtable.h"
+
+// Constructor and destructor for hash table
+HashTable::HashTable(void)
+{
+	buildTable();
+}
+
+HashTable::~HashTable(void)
+{
+	emptyTable();
+}
+
+
+void HashTable::createTable(void)
+{
+	for (int i = 0; i < tableSize; i++)
+	{
+		table[i].head = NULL;
+	}
+}
+
+void HashTable::emptyTable(void)
+{
+	// loop through hashstore
+	for (int i = 0; i < tableSize; i++)
+	{
+		if (table[i].head != NULL)
+		{
+			// next iterate through list deleting data and node.
+			hashItems* current = table[i].head;
+			while (table[i].head != NULL)
+			{
+				table[i].head = table[i].head->next;
+				if (current->custRecord != NULL) {						// test if node's object is null
+					delete current->custRecord;           				// delete the node object
+					current->custRecord = NULL;
+				}
+				delete current;											// then delete node
+				current = NULL;
+				current = table[i].head;	  
+			}
+		
+		}
+	}
+}
+
+bool HashTable::insertCustomer(Customer* custToInsert) const
+{
+
+}
+
+Customer* HashTable::getCustomer(int custID)
+{
+
+}
+
+
--- hashtable.h
+++ hashtable.h
@@ -1,14 +1,12 @@
 /*-------------------------------------------------------------------------------------------------
 
-	Authors:		Boyer, Destiny
-					Bushey, Luke
+	Authors:		Bushey, Luke
 					King, Garret
-					Selin, Zach
 
 	Created:		2/21/2017
 	Modified:		3/1/2017
 
-	Templatized HashTable class. Class has functionality to populate a HashTable, retrieve items,
+	HashTable class that has the functionality to populate a HashTable, retrieve items,
 	and insert items.
 
 	NOTES:	All responsibility for handling a filestream to populate an Object's data members
@@ -20,61 +18,40 @@
 #ifndef hashtable_h
 #define hashtable_h
 
+#include <iostream>
+#include "customer.h"
+
 using namespace std;
 
-template<class Object>
+
 class HashTable {
+	static const int tableSize = 100;
+	static const int tableHash = 101;
 
-	public:
+public:
 	
-	HashTable(void);		//default no-args constructor
-	~HashTable(void);		//destructor
-	
-	/*------------------------------------------------------------------------------------------------
+	HashTable(void);					// default no-args constructor
+	~HashTable(void);					// destructor
 
-		Method takes in a file stream and passes the file stream to the objects to populate
-		objects and then insert them into the HashTable.
+	Customer* getCustomer(int) const;					// method uses customer id and returns a pointer to that customer obj.
+	bool insertCustomer(Customer*);		// method to populate customer into hash table.
 
-		PRECONDITIONS:
-			- data must be formatted for Objects
+private:
+	void createTable();						    // method for constructor to create table
+	void emptyTable();							// method for destructor to clear table	
 
-		POSTCONDITIONS:
-			- populates HashTable with objects
+	struct hashItems
+	{
+		Customer* custRecord;
+		hashItems* next;
+	};
 
-		NOTES:	Objects are solely responsible for knowing how to handle data
-			in order to populate data members.
+	struct hashStore
+	{
+		hashItems* head;
+	};
 
-	------------------------------------------------------------------------------------------------*/
-
-	bool populateTable(ifstream& inFile, HashTable* toPopulate);
-
-	/*------------------------------------------------------------------------------------------------
-
-		Method takes in a target Object and finds the corresponding Object in the HashTable. If
-		the Object is found retrieved is set equal to the object.
-
-		POSTCONDITIONS:
-			- sets retrieved to retrieved Object.
-
-		NOTES:	Objects are solely responsible equality comparisons
-
-	------------------------------------------------------------------------------------------------*/
-
-	Object* retrieve(const Object* target, Object* retrieved) const;
-
-	/*------------------------------------------------------------------------------------------------
-
-		Method takes in an Object and inserts it into the table. Returns a bool indicating success.
-
-		PRECONDITIONS:
-			- toInsert cannot be nullptr
-
-		POSTCONDITIONS:
-			- inserts toInsert into HashTable
-
-	------------------------------------------------------------------------------------------------*/
-
-	bool insert(Object* toInsert);
+	hashStore table[tableSize];
 
 };
 
